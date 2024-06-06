84dc8f704d86f1dc912424a9d66c5405c200d841
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-06-06 17:29:03 -0600

    implementación con postgresql

---
+++ src/main/java/com/example/datosposgre/DatosposgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.datosposgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatosposgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatosposgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datosposgre/DatosposgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datosposgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatosposgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
